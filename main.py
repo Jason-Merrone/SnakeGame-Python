85a937fbc6b7281e3a0df77bc24d3d13f9a02e91
Jason Merrone <jasonmerrone@gmail.com>
2026-05-04 01:09:40 -0600

    Added controls and more efficient movement

--- main.py
+++ main.py
@@ -13,9 +13,17 @@ game_is_on = True
 
 snake = Snake()
 
+# Keypress logic
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 while game_is_on:
   screen.update()
-  time.sleep(.5)
+  time.sleep(.1)
+
   snake.move()
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,23 +1,53 @@
+from collections import deque
+import copy
 from turtle import Turtle
 
 
-STARTING_POS = [(0,0), (-20,0), (-40,0)]
+STARTING_POS = [(-40,0), (-20,0), (0,0)]
+MOVE_DISTANCE = 20
 
 class Snake:
 
   def __init__(self):
-    self.segments = []
-    self.create_snake()
+    self.segments = deque()
+    self._create_snake()
+    self._head = self.segments[-1]
+
+    self._dir = "right"
+    self._vertical = {"up","down"}
+    self._horizontal = {"left","right"}
   
-  def create_snake(self):
-    for pos in STARTING_POS:
+  def _create_snake(self):
+    for i in range(len(STARTING_POS)):
       new_segment = Turtle(shape="square")
       new_segment.color("white")
       new_segment.penup()
-      new_segment.goto(pos)
+      new_segment.goto(STARTING_POS[i])
       self.segments.append(new_segment)
     
   def move(self):
-    for i in range(len(self.segments)-1, 0, -1):
-      self.segments[i].goto(self.segments[i-1].position())
-    self.segments[0].forward(20)
+    self.segments[0].goto(self._head.position())
+    self.segments[0].setheading(self._head.heading())
+    self.segments.append(self.segments.popleft())
+    self._head = self.segments[-1]
+    self._head.forward(MOVE_DISTANCE)
+  
+  def up(self):
+      if self._dir in self._horizontal:
+         self._head.setheading(90)
+         self._dir = "up"
+
+  def down(self):
+      if self._dir in self._horizontal:
+         self._head.setheading(270)
+         self._dir = "down"
+
+  def left(self):
+      if self._dir in self._vertical:
+         self._head.setheading(180)
+         self._dir = "left"
+
+  def right(self):
+      if self._dir in self._vertical:
+         self._head.setheading(0)
+         self._dir = "right"
